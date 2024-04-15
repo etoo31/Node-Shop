16ae3ed2778925e3f7bce1765788d0a476336355
Youssef Ashraf <youssefashrafeissa@gmail.com>
2024-04-15 12:18:32 +0200

    added a middleware fuction isAuth that check if the user is authenticated and handle routes authority

--- routes/admin.js
+++ routes/admin.js
@@ -4,15 +4,17 @@ const express = require("express");
 // custom modules
 const adminController = require("../controllers/admin");
 
+const isAuth = require("../middleware/isAuth");
+
 const router = express.Router();
 
-router.get("/add-product", adminController.getAddProduct);
+router.get("/add-product", isAuth, adminController.getAddProduct);
 
-router.post("/add-product", adminController.addProduct);
+router.post("/add-product", isAuth, adminController.addProduct);
 
-router.get("/products", adminController.getAdminProducts);
-router.get("/edit-product/:productID", adminController.getEditProduct);
-router.post("/edit-product", adminController.updateProduct);
-router.post("/delete-product", adminController.deleteProduct);
+router.get("/products", isAuth, adminController.getAdminProducts);
+router.get("/edit-product/:productID", isAuth, adminController.getEditProduct);
+router.post("/edit-product", isAuth, adminController.updateProduct);
+router.post("/delete-product", isAuth, adminController.deleteProduct);
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -4,16 +4,21 @@ const path = require("path");
 
 // custome modules
 const productsController = require("../controllers/shop");
+const isAuth = require("../middleware/isAuth");
 const router = express.Router();
 
 router.get("/", productsController.getIndex);
 
 router.get("/products", productsController.getProducts);
-router.get("/products/:productID", productsController.getProductDetails);
-router.get("/cart", productsController.getCart);
-router.get("/orders", productsController.getOrders);
+router.get(
+  "/products/:productID",
+  isAuth,
+  productsController.getProductDetails
+);
+router.get("/cart", isAuth, productsController.getCart);
+router.get("/orders", isAuth, productsController.getOrders);
 // // router.get("/checkout", productsController.getCart);
-router.post("/cart", productsController.addToCart);
-router.post("/delete-cart-item", productsController.deleteCartItem);
-router.post("/checkout", productsController.postCheckout);
+router.post("/cart", isAuth, productsController.addToCart);
+router.post("/delete-cart-item", isAuth, productsController.deleteCartItem);
+router.post("/checkout", isAuth, productsController.postCheckout);
 module.exports = router;

6a1653284d485c12f59d1dfd8372f7596f8af8eb
Youssef Ashraf <youssefashrafeissa@gmail.com>
2024-04-26 21:37:09 +0300

    feat : adding validation to sign up using express-validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { render } = require("ejs");
 const bcrypt = require("bcryptjs");
 const sendMail = require("../utils/transporter").sendMail;
 const crypto = require("crypto");
+const { validationResult } = require("express-validator");
 require("dotenv").config();
 
 const User = require("../models/user");
@@ -71,43 +72,41 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPasswrod = req.body.confirmPasswrod;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render("auth/signup", {
+      pageTitle: "Sign up",
+      path: "/signup",
+      errorMessage: errors.array()[0].msg,
+    });
+  }
 
-  User.findOne({ email: email })
-    .then((user) => {
-      if (user) {
-        req.flash(
-          "error",
-          "E-mail is already exist, please pick up another one"
-        );
-        return res.redirect("/signup");
-      }
-      bcrypt
-        .hash(password, 12)
-        .then((hashedPass) => {
-          const newUser = User({ email: email, password: hashedPass });
-          return newUser.save();
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPass) => {
+      const newUser = User({ email: email, password: hashedPass });
+      return newUser.save();
+    })
+    .then((result) => {
+      res.redirect("/login");
+      const mailOptions = {
+        from: {
+          name: "Your Shop",
+          address: process.env.MAIL,
+        }, // sender address
+        to: email, // list of receivers
+        subject: "Sending email via nodemailer for gmail only ✔", // Subject line
+        text: "Hello world?", // plain text body
+        html: "<b>Hello world?</b>", // html body
+      };
+      sendMail(mailOptions)
+        .then((_) => {
+          console.log("MAIL has been snet to : ", email);
         })
-        .then((result) => {
-          res.redirect("/login");
-          const mailOptions = {
-            from: {
-              name: "Your Shop",
-              address: process.env.MAIL,
-            }, // sender address
-            to: email, // list of receivers
-            subject: "Sending email via nodemailer for gmail only ✔", // Subject line
-            text: "Hello world?", // plain text body
-            html: "<b>Hello world?</b>", // html body
-          };
-          sendMail(mailOptions)
-            .then((_) => {
-              console.log("MAIL has been snet to : ", email);
-            })
-            .catch((err) => {
-              console.log("error sending email , ", err);
-            });
-        })
-        .catch((err) => console.log(err));
+        .catch((err) => {
+          console.log("error sending email , ", err);
+        });
     })
     .catch((err) => console.log(err));
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,43 @@
 // core modules
 const express = require("express");
+const { check, body } = require("express-validator");
 
 // custome modules
 const authController = require("../controllers/auth");
+const User = require("../models/user");
 const router = express.Router();
 
 router.get("/login", authController.getLogin);
 router.post("/login", authController.postLogin);
 router.post("/logout", authController.postLogout);
 router.get("/signup", authController.getSignup);
-router.post("/signup", authController.postSignup);
+router.post(
+  "/signup",
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter a valid email address.")
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then((user) => {
+          if (user) {
+            return Promise.reject("This email is already exist !!");
+          }
+        });
+      }),
+    body("password")
+      .isLength({
+        min: 5,
+      })
+      .withMessage("Password must be atleast of length 5"),
+    body("confirmPassword").custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Passwords must match !!");
+      }
+      return true;
+    }),
+  ],
+  authController.postSignup
+);
 router.get("/reset", authController.getReset);
 router.post("/reset", authController.postReset);
 router.get("/reset/:token", authController.getNewPassordPage);

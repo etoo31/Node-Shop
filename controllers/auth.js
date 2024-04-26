dd133f682cc3633e18e5bf073c70966c8741c79f
Youssef Ashraf <youssefashrafeissa@gmail.com>
2024-04-27 00:59:08 +0300

    feat : keeping user input even it's error and displaying border of red color on in the input that have the error on signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,7 +6,7 @@ const { validationResult } = require("express-validator");
 require("dotenv").config();
 
 const User = require("../models/user");
-const user = require("../models/user");
+
 exports.getLogin = (req, res, next) => {
   let message = req.flash("error");
   if (message.length > 0) {
@@ -75,11 +75,18 @@ exports.getSignup = (req, res, next) => {
     pageTitle: "Sign up",
     path: "/signup",
     errorMessage: message,
+    oldInput: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    errors: [],
   });
 };
 exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
 
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
@@ -88,6 +95,12 @@ exports.postSignup = (req, res, next) => {
       pageTitle: "Sign up",
       path: "/signup",
       errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email: email,
+        password: password,
+        confirmPassword: confirmPassword,
+      },
+      errors: errors.array(),
     });
   }
 
